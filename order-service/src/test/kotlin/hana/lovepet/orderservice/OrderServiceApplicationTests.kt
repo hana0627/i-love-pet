7df81ea73b533bf34822f612e1569c1900aa7819
hana0627 <hanana9506@naver.com>
2025-07-28 16:18:14 +0900

    #3 order-service Initial commit

---
+++ order-service/src/test/kotlin/hana/lovepet/orderservice/OrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hana.lovepet.orderservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
