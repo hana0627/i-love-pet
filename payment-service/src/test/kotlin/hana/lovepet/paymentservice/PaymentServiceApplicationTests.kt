c1141185d4819e62cdcb7a593b5061422338c845
hana0627 <hanana9506@naver.com>
2025-08-01 11:59:47 +0900

    #4 payment-service Initial commit

---
+++ payment-service/src/test/kotlin/hana/lovepet/paymentservice/PaymentServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hana.lovepet.paymentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
