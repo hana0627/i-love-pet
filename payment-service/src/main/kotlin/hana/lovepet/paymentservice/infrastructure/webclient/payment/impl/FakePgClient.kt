235defb4aa08c304d52d6c4805810f9903eecd2d
hana0627 <hanana9506@naver.com>
2025-08-03 17:48:37 +0900

    #4 payment service 통신성공 확인 및 docker file 작성

--- payment-service/src/main/kotlin/hana/lovepet/paymentservice/infrastructure/webclient/payment/impl/FakePgClient.kt
+++ payment-service/src/main/kotlin/hana/lovepet/paymentservice/infrastructure/webclient/payment/impl/FakePgClient.kt
@@ -1,5 +1,6 @@
 package hana.lovepet.paymentservice.infrastructure.webclient.payment.impl
 
+import hana.lovepet.paymentservice.common.uuid.UUIDGenerator
 import hana.lovepet.paymentservice.infrastructure.webclient.payment.PgClient
 import hana.lovepet.paymentservice.infrastructure.webclient.payment.dto.request.PgApproveRequest
 import hana.lovepet.paymentservice.infrastructure.webclient.payment.dto.response.PgApproveResponse
@@ -7,13 +8,15 @@ import org.springframework.stereotype.Component
 import kotlin.random.Random
 
 @Component
-class FakePgClient: PgClient {
+class FakePgClient(
+    private val uuidGenerator: UUIDGenerator,
+): PgClient {
     override fun approve(request: PgApproveRequest): PgApproveResponse {
 
         // 30% 확률로 결제실패
         return if (Random.nextInt(3) == 0) {
             PgApproveResponse.fail(
-                paymentKey = "5EnNZRJGvaBX7zk2yd8ydw26XvwXkLrx9POLqKQjmAw4b0e1",
+                paymentKey = "fail_${uuidGenerator.generate()}",
                 failReason = "한도초과",
                 rawJson = """{"code":"LIMIT_OVER","message":"한도초과"}"""
             )
@@ -21,7 +24,7 @@ class FakePgClient: PgClient {
 
         else {
             PgApproveResponse.success(
-                paymentKey = "5EnNZRJGvaBX7zk2yd8ydw26XvwXkLrx9POLqKQjmAw4b0e1",
+                paymentKey = "success_${uuidGenerator.generate()}",
                 amount = request.amount,
                 method = request.method ?: "카드",
                 rawJson = "{\"result\":\"ok\"}"
