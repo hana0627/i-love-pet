89c7f86700edd751e1a8d68231f2d821b24c1016
hana0627 <hanana9506@naver.com>
2025-08-19 17:40:44 +0900

    #8 토스페이먼츠 연동하기 - 결제위젯 띄우기

--- front/src/pages/orders/OrderCreate.jsx
+++ front/src/pages/orders/OrderCreate.jsx
@@ -1,16 +1,28 @@
 import {Link, useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import {loadTossPayments} from "@tosspayments/tosspayments-sdk";
 
 function OrderCreate() {
+  const clientKey = process.env.REACT_APP_TOSS_CLIENT_KEY;
+
   const nav = useNavigate();
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
   const [products, setProducts] = useState([]);
-  const [selected, setSelected] = useState({}); // { [productId]: quantity }
+  const [selected, setSelected] = useState({});
   const [submitting, setSubmitting] = useState(false);
 
-  useEffect(() => {
+  // 토스페이먼츠 - start
+  const [ready, setReady] = useState(false);
+  const [widgets, setWidgets] = useState(null);
 
+  const [openWidget, setOpenWidget] = useState(false);
+
+  const renderedRef = useRef(false);
+  // 토스페이먼츠 - end
+
+
+  useEffect(() => {
     getUsers().then(data => {
         setUsers(data)
         if (data.length > 0) setSelectedUser(data[0].userId);
@@ -73,49 +85,117 @@ function OrderCreate() {
   const selectedItems = products
     .filter((p) => selected[p.productId] != null)
     .map((p) => ({
-      productId: p.productId ?? p.id ?? p._id ?? p.name,
-      productName: p.productName ?? p._productName,
+      productId: p.productId,
+      productName: p.productName,
       price: Number(p.price),
       quantity: Number(selected[p.productId])
     }));
 
   const total = selectedItems.reduce((sum, it) => sum + (it.price * it.quantity), 0);
 
-  async function submitOrder() {
-    if (submitting) return;
-    if (!selectedUser) return alert("사용자를 선택해주세요.");
-    if (selectedItems.length === 0) return alert("상품을 선택하고 수량을 입력해주세요.");
-
-    setSubmitting(true);
-    try {
-      const res = await fetch("http://localhost:8082/api/orders", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            userId: Number(selectedUser),
-            items: selectedItems
-          }
-        ),
+  // 토스페이먼츠 - start
+  // 토스페이먼츠 SDK 객체 불러오기
+  useEffect(() => {
+    async function fetchPaymentWidgets() {
+      if (!clientKey || !selectedUser) return;
+      // ------  결제위젯 초기화 ------
+      const tossPayments = await loadTossPayments(clientKey);
+      const widget = tossPayments.widgets({
+        // customerKey는 구매자를 구분하는 값.
+        customerKey: `${selectedUser}_`,
       });
 
-      if (!res.ok) {
-        let msg = "주문 생성 실패";
-        try {
-          const err = await res.json();
-          if (err?.message) msg = err.message;
-        } catch {
-        }
-        alert(msg);
-        return;
-      }
-      const data = await res.json();
-      alert(`주문 생성 완료! 주문번호: ${data.orderId}`);
-      nav("/");
-    } catch (e) {
-      alert("주문 생성 중 오류가 발생했습니다.");
-    } finally {
-      setSubmitting(false);
+      setWidgets(widget);
     }
+
+    fetchPaymentWidgets();
+  }, [clientKey, selectedUser]);
+
+
+  // 토스페이먼츠 위젯 그리기
+  async function renderPaymentWidgets() {
+    if (!widgets || renderedRef.current) return;
+    await Promise.all([
+      // ------  결제 UI 렌더링 ------
+      widgets.renderPaymentMethods({
+        selector: "#payment-method",
+        variantKey: "DEFAULT",
+      }),
+      // ------  이용약관 UI 렌더링 ------
+      widgets.renderAgreement({
+        selector: "#agreement",
+        variantKey: "AGREEMENT",
+      }),
+    ]);
+    setReady(true);
+    renderedRef.current = true;
+  }
+  // 토스페이먼츠 - end
+
+  // 토스페이먼츠 결제요청 - start
+  function requestPayments() {
+
+    // 여기서 서버호출
+    // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+    // --> orderId 생성하기.
+
+    // 아래는 기존에 Fake 결제를 위한 api
+    // try {
+    //   const res = await fetch("http://localhost:8082/api/orders", {
+    //     method: "POST",
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify({
+    //         userId: Number(selectedUser),
+    //         items: selectedItems
+    //       }
+    //     ),
+    //   });
+    //
+    //   if (!res.ok) {
+    //     let msg = "주문 생성 실패";
+    //     try {
+    //       const err = await res.json();
+    //       if (err?.message) msg = err.message;
+    //     } catch {
+    //     }
+    //     alert(msg);
+    //     return;
+    //   }
+    //   const data = await res.json();
+    //   alert(`주문 생성 완료! 주문번호: ${data.orderId}`);
+    //   nav("/");
+    // } catch (e) {
+    //   alert("주문 생성 중 오류가 발생했습니다.");
+    // } finally {
+    //   setSubmitting(false);
+    // }
+
+
+    widgets.requestPayment({
+      orderId: `demo_${Date.now()}`,
+      orderName: selectedItems.length === 1
+        ? selectedItems[0].productName
+        : `${selectedItems[0].productName} 외 ${selectedItems.length - 1}건`,
+      successUrl: window.location.origin + "/success",
+      failUrl: window.location.origin + "/fail",
+    })
+  }
+
+  // 토스페이먼츠 결제요청 - end
+
+
+  async function createOrder() {
+    if (submitting) return;
+    setSubmitting(true);
+
+    if (!selectedUser) return alert("사용자를 선택해주세요.");
+    if (selectedItems.length === 0) return alert("상품을 선택하고 수량을 입력해주세요.");
+    await widgets.setAmount({currency: "KRW", value: total});
+
+    setOpenWidget(true)
+    await renderPaymentWidgets()
+
+    setSubmitting(false);
   }
 
   return (
@@ -166,17 +246,57 @@ function OrderCreate() {
                          style={{width: "100%", padding: "8px 10px", border: "1px solid #cbd5e1", borderRadius: 12}}
                   />
                 </td>
+
               </tr>
             );
           })}
           </tbody>
         </table>
 
+        <div
+          style={{
+            position: 'fixed', inset: 0, background: 'rgba(0,0,0,.45)',
+            display: openWidget ? 'flex' : 'none',   // ← 여기만 바뀜
+            alignItems: 'center', justifyContent: 'center', zIndex: 1000
+          }}
+          onClick={() => setOpenWidget(false)}>
+          <div
+            style={{
+              background: '#fff', borderRadius: 12, padding: 20,
+              width: 600, maxWidth: '92vw', position: 'relative'
+            }}
+            onClick={(e) => e.stopPropagation()}>
+            <button
+              style={{zIndex: 2000}} aria-label="닫기" className="toss-close"
+              onClick={() => {
+                setOpenWidget(false);
+              }}
+            >×
+            </button>
+            <div className="wrapper">
+              <div className="box_section">
+                {/* 결제 UI */}
+                <div id="payment-method"/>
+                {/* 이용약관 UI */}
+                <div id="agreement"/>
+                {/* 결제하기 버튼 */}
+                <button
+                  className="toss-btn primary"
+                  disabled={!ready || total <= 0}
+                  onClick={requestPayments}>
+                  결제하기
+                </button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="flex-between mb-6" style={{marginTop: 12}}>
           <div className="section-title">총액: {total.toLocaleString()}원</div>
           <div style={{display: "flex", gap: 8}}>
             <button className="btn gray" onClick={() => nav(-1)}>뒤로</button>
-            <button className="btn teal" onClick={submitOrder} disabled={submitting}>주문하기</button>
+            <button className="btn teal" onClick={createOrder} disabled={submitting}>주문하기</button>
           </div>
         </div>
       </div>
